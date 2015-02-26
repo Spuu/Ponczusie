4897c918f40a6b06e2cf62217924db3140eb887a
Spuu <m.ruszewski@gmail.com>
2015-02-26 22:58:59 +0100

    Initial Commit
    Basic mechanics.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 3000;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+		rigidbody.AddForce(move * speed * Time.deltaTime);
+	}
+}
